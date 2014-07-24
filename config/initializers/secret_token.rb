0d2c08903bd773cfa65f3fdfebb66b6aa60bcf78
Amit Patel <CHETU\amitp@chetuiwk430.chetu.com>
2014-07-24 20:18:07 +0530

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '317c7b8d19d03447eb9abcf691e424677327ec97e9147084350031d3769accea0f1946c98b700f93efce3b233896d4786a5f911836ae710ba27425005ef59dae'
